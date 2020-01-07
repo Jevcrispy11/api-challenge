de1df406374cc3b47696d857ba35905c75d600da
Jordan Evans <jevans@linkedin.biz>
2020-01-06 22:44:27 -0600

    Submitting HW

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "83c80914f048b003aed67b867acc1097"
+
+# Google API Key
+g_key = "AIzaSyBKCIY8UhBis0xXX9viFsVEPJ-hElFC_EY"
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "83c80914f048b003aed67b867acc1097"
+
+# Google API Key
+g_key = "AIzaSyDUlWsRCFrvUEJg33YH3NOY8KcHgeDZR5w"
